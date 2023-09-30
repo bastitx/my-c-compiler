ecc37f522dda58c31b5cc9b9508ccfb72ce705e7
bastitx <low.boiler@b11n.de>
2023-09-30 18:26:33 +0200

    cleanup a bit

--- my_compiler.sh
+++ my_compiler.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-/home/bastian/Development/my_compiler/target/debug/my_compiler $1
+PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
+${PROJECT_DIR}/target/release/my_compiler $1
 f=$1
 s_file="${f%.c}.s"
 # o_file="${f%.c}.o"
 file="${f%.c}"
 gcc $s_file -o $file
-# rm $s_file
+rm $s_file
--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,17 @@ use std::{fs, env, path};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // println!("Got {:?} args", args);
     let input = if args.len() == 2 {
         fs::read_to_string(&args[1]).unwrap()
     } else {
         "int main(){\nint a = 0; int b = 0; a && (b = 5); return b; }".to_string()
     };
-    // println!("{}", input);
     let tokens = lexer::lex(input.as_str());
-    // println!("{:?}", tokens);
     let ast = parser::parse(tokens);
-    // println!("{:?}", ast);
     let output = generator::generate(ast);
-    // println!("{}", output);
     if args.len() == 2 {
         let file_path = path::Path::new(&args[1]);
         let output_file = file_path.with_extension("s");
-        // println!("Writing to {:?}", output_file);
         fs::write(output_file, output).unwrap();
     }
 }
